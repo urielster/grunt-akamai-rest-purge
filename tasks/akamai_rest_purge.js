c2fe064d3160628a27f1812a48a21005f78c0c77
urielster <uriel.elster@gmail.com>
2014-05-27 10:47:28 +0300

    Add function to deal with folder and files

--- tasks/akamai_rest_purge.js
+++ tasks/akamai_rest_purge.js
@@ -11,17 +11,31 @@
 var request = require('request');
 
 var formatResponse = function (response) {
-  return response.detail +
-    ' - about ' +
-    response.estimatedSeconds +
-    ' seconds are needed to complete the purge request!'
+  if (response.body){
+    return response.body.detail +
+      ' - about ' +
+      response.body.estimatedSeconds +
+      ' seconds are needed to complete the purge request!'
+  }
+  return "";
 };
 
+var formatCheckStatusResponse = function(response){
+  if (response.body && response.body.progressUri){
+    var statusUrl = "https://api.ccu.akamai.com"+response.body.progressUri;
+    return "Check Status: "+  statusUrl ;
+  }
+  return "";
+};
 module.exports = function(grunt) {
 
   grunt.registerMultiTask('akamai_rest_purge', 'Purging Akamai via their Rest Interface', function() {
 
     var done = this.async();
+    var data = this.data;
+    var matchers  = (data.fileMatch) ||  null ;
+
+    var urls = data.urls || getFoldersFileNames(matchers);
 
     var options = this.options({
       method: 'post',
@@ -34,8 +48,10 @@ module.exports = function(grunt) {
       'domain'
     ]);
 
-    options.json.objects = this.data.objects;
-
+    options.json.objects = urls;
+    if (!urls || (urls instanceof Array && urls.length < 1) ){
+      return grunt.log.errorlns("No files found or matched");
+    }
     request(options, function (err, response) {
       if (err) {
         return grunt.log.errorlns(err.message);
@@ -43,10 +59,40 @@ module.exports = function(grunt) {
       if (response.statusCode !== 201) {
         return grunt.log.errorlns(response.body.detail || response.body);
       }
-      grunt.log.ok(formatResponse(response.body));
+      grunt.log.ok(formatResponse(response));
+      grunt.log.ok(formatCheckStatusResponse(response));
+
+
+
       done();
     });
-
   });
+
+
+
+
+  /**
+   * Let it search fs and get the list of files to invalidate
+   * @param fileMatch
+   * @returns {Array}
+   */
+  var getFoldersFileNames = function(fileMatch){
+
+    var resultUrls = [];
+
+    if (fileMatch && fileMatch !== null && fileMatch.pattern && fileMatch.dest){
+      var options = fileMatch.options;
+      options.rename = function(dest, matchedSrcPath, options) {
+        return (dest +  matchedSrcPath); //instead of using path which is for real paths in fs
+      };
+
+      var map = grunt.file.expandMapping(fileMatch.pattern, fileMatch.dest,fileMatch.options );
+      map.forEach(function(url){
+        resultUrls.push(url.dest);
+      });
+    }
+    return resultUrls;
+
+  }
 
 };
